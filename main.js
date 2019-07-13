d3c97ac577d33fa8d322c65d277a58695812bca2
Kevin Tassi <kevin@fishtripr.com>
2019-07-13 13:38:09 +0200

    Same result with different synthax

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ var Point = /** @class */ (function () {
         this.x = x;
         this.y = y;
     }
-    // by default it's public methods, you can specify it, but not necessary
     Point.prototype.draw = function () {
         console.log('X: ' + this.x + ',Y: ' + this.y);
     };
--- main.ts
+++ main.ts
@@ -1,17 +1,10 @@
 
 
 class Point {
-  // access modifiers to avoid to modify the value out of the class
-  private x: number;
-  private y: number;
-
-  constructor(x?: number, y?: number) {
-    this.x = x;
-    this.y = y;
+  constructor(private x?: number, private y?: number) {
   }
 
-  // by default it's public methods, you can specify it, but not necessary
-  public draw() {
+  draw() {
     console.log('X: ' + this.x + ',Y: ' + this.y);
   }
 
