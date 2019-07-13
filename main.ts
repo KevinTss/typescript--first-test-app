1c8f1f822ff1ae2a0319e99c2df199dcc132525f
Kevin Tassi <kevin@fishtripr.com>
2019-07-13 13:09:46 +0200

    Type object format

--- main.ts
+++ main.ts
@@ -1,10 +1,38 @@
 
-// Arrow functions
+/*
+Problem:
 
-let log = function(message) {
-  console.log(message);
+I you have a function who receive a object as parameter, this object should be formatted as the function needed
+
+Example:
+
+let drawPoint = (point) => {
+  // ...
 }
 
-let doLog = (message) => console.log(message);
+Here we are attenting for this:
 
-// Like JS ES6
+drawPoint({
+  x: 1,
+  y: 2
+})
+
+But we cann pass this:
+
+drawPoint({
+  name: "Kevin"
+})
+
+End the function will not work...
+
+Solution:
+*/
+
+let drawPoint = (point: { x: number, y: number}) => {
+  // ...
+}
+
+// We just specify the format of the object that the function is waiting for
+
+// But... Is verbose...
+// The solution is use interface
