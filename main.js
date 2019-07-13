5642dfe84f2d4bb7e13c0f0dab6d184fbcafcc10
Kevin Tassi <kevin@fishtripr.com>
2019-07-13 13:02:38 +0200

    Arrow functions

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
-// Type assertion
-var message;
-message = 'abc';
-var isEndsWithC = message.endsWith('c');
-// the <string> allow us to tell TS that the following variable is a string
-// Then you can have the intellisense with the methods `endsWith` who is aviable only on a string variable
-// Other way to do it
-var isEndsWithC_2 = message.endsWith('c');
-// These both methods won't change the type of the variable. It's just a way to tell TS that this specific cariable is this type
+// Arrow functions
+var log = function (message) {
+    console.log(message);
+};
+var doLog = function (message) { return console.log(message); };
+// Like JS ES6
--- main.ts
+++ main.ts
@@ -1,14 +1,10 @@
 
-// Type assertion
+// Arrow functions
 
-let message;
-message = 'abc';
-let isEndsWithC = (<string>message).endsWith('c');
-// the <string> allow us to tell TS that the following variable is a string
-// Then you can have the intellisense with the methods `endsWith` who is aviable only on a string variable
+let log = function(message) {
+  console.log(message);
+}
 
-// Other way to do it
+let doLog = (message) => console.log(message);
 
-let isEndsWithC_2 = (message as string).endsWith('c');
-
-// These both methods won't change the type of the variable. It's just a way to tell TS that this specific cariable is this type
+// Like JS ES6
