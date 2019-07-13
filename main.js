3bc2183baf017833e5333845e59d9fbef9666293
Kevin Tassi <kevin@fishtripr.com>
2019-07-13 13:52:29 +0200

    Compilation of modules

--- main.js
+++ main.js
@@ -1,26 +1,5 @@
-var Point = /** @class */ (function () {
-    function Point(_x, _y) {
-        this._x = _x;
-        this._y = _y;
-    }
-    Point.prototype.draw = function () {
-        console.log('X: ' + this._x + ',Y: ' + this._y);
-    };
-    Object.defineProperty(Point.prototype, "x", {
-        get: function () {
-            return this._x;
-        },
-        set: function (value) {
-            if (value < 0)
-                throw new Error('value cannot be less than 0');
-            this._x = value;
-        },
-        enumerable: true,
-        configurable: true
-    });
-    return Point;
-}());
-var a = new Point(1, 2);
-var xValue = a.x;
-a.x = 10;
+"use strict";
+exports.__esModule = true;
+var point_1 = require("./point");
+var a = new point_1.Point(1, 2);
 a.draw();
