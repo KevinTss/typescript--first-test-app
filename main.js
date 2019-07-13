55c074609e43f3e7c2540808727a7df2111e0da6
Kevin Tassi <kevin@fishtripr.com>
2019-07-13 12:50:01 +0200

    Different type of variable

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
-function log(message) {
-    console.log(message);
-}
-var message = "Hello Word";
-log(message);
+var a;
+var b;
+var c;
+var d; // Any types
+var e = [1, 2, 3]; // Decalre a array of numbers
+var f = ["a", true, 1]; // Decalre an array of any ==> NOT RECOMMENDED
+// ENUM
+var ColorRed = 0;
+var ColorGreen = 1;
+var ColorBlue = 2;
+// this can be done using enum type from typescript
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+;
+var backgroundColor = Color.Red;
+console.log(backgroundColor)
--- main.ts
+++ main.ts
@@ -1,4 +1,21 @@
-let a: String;
 
-a = "Kevin";
-a = 123;
+let a: string;
+
+let b: number;
+
+let c: boolean;
+
+let d: any; // Any types
+
+let e: number[] = [1, 2, 3]; // Decalre a array of numbers
+
+let f: any[] = ["a", true, 1]; // Decalre an array of any ==> NOT RECOMMENDED
+
+// ENUM
+const ColorRed = 0;
+const ColorGreen = 1;
+const ColorBlue = 2;
+// this can be done using enum type from typescript
+
+enum Color { Red = 0, Green = 1, Blue = 2 };
+let backgroundColor = Color.Red;
